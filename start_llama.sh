1e966b1f50a670f13d0d6070444e8257b0a728ce
Daniel Todd <todddaniel@gmail.com>
2025-09-14 09:46:11 +0100

    Make enabling/disabling Vulkan/Cuda servers easier for when you just need one of them.

--- start_llama.sh
+++ start_llama.sh
@@ -4,31 +4,24 @@
 source ./llama_config.env
 
 #### VULKAN version
+if [ "$ENABLE_VULKAN" = true ]; then
+  ## E.g. gpt-oss-20b on Vulkan
+  llama-amd \
+    -m "$VULKAN_MODEL_PATH" \
+    -fa on \
+    -c "$VULKAN_CONTEXT" \
+    --host "$VULKAN_HOST" \
+    --port "$VULKAN_PORT" \
+    --grammar-file "$VULKAN_GRAMMAR_FILE" &
+fi
 
-## gpt-oss-20b on Vulkan
-# GGML_VULKAN_DEVICE=0 llama-amd \
-#   -m "$VULKAN_MODEL_PATH" \
-#   -fa on \
-#   -c "$VULKAN_CONTEXT" \
-#   --host "$VULKAN_HOST" \
-#   --port "$VULKAN_PORT" \
-#   --grammar-file "$VULKAN_GRAMMAR_FILE" &
-
-# CUDA version
-
-## devstral:20b on CUDA
-CUDA_VISIBLE_DEVICES=0 llama-nvidia \
- -m "$CUDA_MODEL_PATH" \
- -fa on \
- -c "$CUDA_CONTEXT" \
- --host "$CUDA_HOST" \
- --port "$CUDA_PORT" &
-
-## gpt-oss-20b on CUDA
-# CUDA_VISIBLE_DEVICES=0 llama-nvidia \
-#   -m "$CUDA_MODEL_PATH" \
-#   --grammar-file "$CUDA_GRAMMAR_FILE" \
-#   --host "$CUDA_HOST" \
-#   --port "$CUDA_PORT" \
-#   -c "$CUDA_CONTEXT" \
-#   --flash-attn on &
+#### CUDA version
+if [ "$ENABLE_CUDA" = true ]; then
+  ## E.g.devstral:20b on CUDA
+  CUDA_VISIBLE_DEVICES=0 llama-nvidia \
+    -m "$CUDA_MODEL_PATH" \
+    -fa on \
+    -c "$CUDA_CONTEXT" \
+    --host "$CUDA_HOST" \
+    --port "$CUDA_PORT" &
+fi
